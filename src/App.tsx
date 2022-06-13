f8eba2529d4cb6f1268d5a1a991ce11e84dd0364
etun <etun@technodom.kz>
2022-06-13 22:18:38 +0600

    add input component

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,15 @@ import './App.scss';
 import Container from './components/Container/Container';
 import Half from './components/Half/Half';
 import Input from './components/Input/Input';
-import Typography from './components/Typography/Typography';
 
 function App() {
   return (
     <div className="app">
       <div className='app__header'>
-        <Typography spacing color='gray' tag='big' bolded>SPLI</Typography>
-        <Typography spacing color='gray' tag='big' bolded>TTER</Typography>
+        <img src='/images/logo.svg' alt='logo'/>
       </div>
       <Container>
-        <Half background='white'><Input/></Half>
+        <Half background='white'><Input logoPath='images/icon-dollar.svg' label='Bill'/></Half>
         <Half background='dark-gray'>dfgfdg</Half>
       </Container>
     </div>
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import clsx from 'clsx'
+import { InputProps } from './Input.props'
+import './Input.scss'
 
-const Input = ():JSX.Element => {
-  return (
-    <input/>
-  )
+const Input = ({ logoPath, label, ...props }: InputProps): JSX.Element => {
+    return (
+        <div className='input' >
+            <span className='input__label'>{label}</span>
+            <div className='input__input-holder'>
+                <img className='input__icon' src={logoPath} alt='icon'/>
+                <input placeholder='0' type={'number'} className={clsx('input__element', {})}  {...props} />
+            </div>
+        </div>
+    )
 }
 
 export default Input
